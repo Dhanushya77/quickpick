1007c4148b8db84875d2ff72dcbb15ec0e3b6336
dhanushya77 <dhanushyavs@gmail.com>
2025-02-17 15:54:55 +0530

    login completed

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('login',views.login),
+    path('',views.dummy_home),
+    path('login',views.user_login),
+    path('logout',views.user_logout),
+    path('shop_home',views.shop_home),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from.models import *
+import os
+from django.contrib.auth.models import User
 # Create your views here.
 
-def login(req):
+def user_login(req):
+    if 'shop' in req.session:
+        return redirect (shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
+    if req.method == 'POST':
+        uname = req.POST['uname']
+        password = req.POST['password']
+        shop = authenticate(username=uname,password=password)
+        if shop:
+            login(req,shop)
+            if shop.is_superuser:
+                req.session['shop'] = uname
+                return redirect(shop_home)
+            else:
+                req.session['user'] = uname
+                return redirect(user_home)
+        else:
+            messages.warning(req,'Invalid username or password')
+            return redirect(user_login)
+    else:
+        return render(req,'login.html')
+    
 
-    return render(req,'login.html')
-
-def home(req):
+def user_logout(req):
+    if 'user' or 'shop' in req.session:
+        logout(req)
+        req.session.flush()
+        return redirect(user_login)
+    else:
+        return redirect(user_login)
+    
+def dummy_home(req):
 
     return render(req,'dummy_home.html')
+
+
+# ----------------------shop--------------------------
+
+def shop_home(req):
+
+    return render(req,'shop/home.html')
+
+
+# ---------------------user----------------------------
+
+def user_home(req):
+
+    return render(req,'user/home.html')
+
